f927f808241b7656cd14e655e1e4199bc1ee0e33
caerphoto <github@caerphoto.com>
2010-11-12 19:23:23 +0000

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reqr::Application.config.secret_token = '7241b3a5c4c28e1465c3a81f3c5298d77031cd483ec6fb41d427eda9208eb147e8ac03dd8cf4b64dacb915b61e5722d7435221adbbb02d50db8d7b7b89e3ba68'
